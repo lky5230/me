e8af43e2d9df351e72c1e249806965ceeea67a19
kaiyuan.liu <kaiyuan.liu@tianheng-ustc.com>
2018-05-24 17:42:57 +0800

    优化

--- src/components/database/database.example.vue
+++ src/components/database/database.example.vue
@@ -1,12 +1,19 @@
 <template>
   <div style="width: 100%;">
       <button style="padding: 6px; border: 1px solid #666;" @click="getTable">获取表格信息21</button>
+
       <button style="padding: 6px; border: 1px solid #666;" @click="checkedRows">获取选中的行</button>
+
       <button style="padding: 6px; border: 1px solid #666;" @click="add">增加一行</button>
+
       <button style="padding: 6px; border: 1px solid #666;" @click="xls">导出xls文件</button>
+
       <button style="padding: 6px; border: 1px solid #666;" title="第一列设置了required=true" @click="checkRequired">对required=true检查</button>
+
       <button style="padding: 6px; border: 1px solid #666;" @click="reset">doReset</button>
+      
       注意：表格宽高是撑满父容器的
+
       <div style="width: 100%; height: 540px; margin-top: 14px;">
         <database
           ref="database"
@@ -32,6 +39,7 @@
         >
         </database>
       </div>
+
   </div>
 </template>
 
--- src/util/download/download.js
+++ src/util/download/download.js
@@ -1,34 +1,22 @@
-/*
-* 文件流js下载文件实例：（注意：请求的responseType 必须是 arrayBuffer）
-*/
-function handlePreview(file){
-    if (!file.id) return;
-    let _this = this;
-    this.downloadLoading = true;
-    downloadContract(file.id).then(res => {
-        this.downloadLoading = false;
-        //res.data是文件流
-        downloadFile(file.name, res.data);
-    }).catch(err => {
-        console.error(err)
-        this.downloadLoading = false;
-    })
-    function downloadFile(fileName, content) {
-        if ('msSaveOrOpenBlob' in window.navigator) {
-            //ie10~11、edge
-            let blob = new Blob([content]);
-            window.navigator.msSaveOrOpenBlob(blob, fileName);
-        } else {
-            let aLink = window.document.createElement('a');
-            let blob = new Blob([content]);
-            let url = window.URL.createObjectURL(blob);
-            aLink.download = fileName;
-            aLink.href = url;
-            aLink.click();
-            _this.$nextTick(() => {
-                window.URL.revokeObjectURL(url);
-                aLink = null;
-            });
-        };
+/**
+ * 参数1：[ 文件名称 ]
+ * 参数2：[ 传输的文件流，类型：application/octet-stream]
+ * 注意： 返回请求的responseType 必须是 arrayBuffer 才行
+ */
+export function downloadFile(fileName, content) {
+    if ('msSaveOrOpenBlob' in window.navigator) {
+        //ie10~11、edge
+        let blob = new Blob([content]);
+        window.navigator.msSaveOrOpenBlob(blob, fileName);
+    } else {
+        let aLink = window.document.createElement('a');
+        let blob = new Blob([content]);
+        let url = window.URL.createObjectURL(blob);
+        aLink.download = fileName;
+        aLink.href = url;
+        aLink.click();
+        
+        window.URL.revokeObjectURL(url);
+        aLink = null;
     };
-}
+};
